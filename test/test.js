e86b4e30125586c12eed19a24ab12f64a403dc81
Ken Ahrens <ken.ahrens@gmail.com>
2016-04-25 18:01:51 -0400

    adding Insights key

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
 var api = require('../api.js');
+var insights = require('../insights.js');
+
 var assert = require('assert');
 
 describe('newrelic-api-test', function() {
@@ -8,7 +10,7 @@ describe('newrelic-api-test', function() {
   beforeEach(function(){
 
   });
-  it('calls the api', function() {
+  it('calls the application api', function() {
     var callback = function(error, response, body) {
       assert.equal(error, null);
       assert.equal(response.statusCode, 200);
@@ -20,4 +22,26 @@ describe('newrelic-api-test', function() {
   after(function() {
 
   });
-})
+});
+
+describe('insights-test', function() {
+  before(function(){
+
+  });
+  beforeEach(function(){
+
+  });
+  it('calls the query api', function() {
+    var callback = function(error, response, body) {
+      assert.equal(error, null);
+      assert.equal(response.statusCode, 200);
+    }
+
+    // Read config file
+    api.applicationList(callback);
+  });
+  after(function() {
+
+  });
+});
+

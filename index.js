54e2055257f3dd7b2ab2b9367738fe9f171b0768
Ken Ahrens <ken.ahrens@gmail.com>
2016-05-06 07:12:04 -0400

    adding synthetics to main test output

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var api = require('./api.js');
 var insights = require('./insights.js');
+var synthetics = require('./synthetics.js');
 
 // Callback
 var appCB = function(error, response, body) {
   if (!error && response.statusCode === 200) {
+    console.log('\n*** APM API output:');
     var applications = body.applications;
     for(var i=0; i < applications.length; i++) {
       var app = applications[i];
@@ -26,7 +28,8 @@ var appCB = function(error, response, body) {
 var nrql = 'SELECT count(*) FROM Transaction';
 var insightsCB = function(error, response, body) {
   if (!error && response.statusCode === 200) {
-    console.log('Insights Transactions = ' + body.results[0].count);
+    console.log('\n*** Insights API output:');
+    console.log('Transaction count = ' + body.results[0].count);
   } else {
     console.log('Insights API Error!');
     if (error) {
@@ -37,6 +40,28 @@ var insightsCB = function(error, response, body) {
   }
 }
 
+var syntheticsCB = function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    console.log('\n*** Synthetics API output:');
+    var monitors = body.monitors;
+    for (var i=0; i < monitors.length; i++) {
+      var name = monitors[i].name;
+      var status = monitors[i].status;
+
+      console.log(name + ' (' + status + ')');
+    }
+  } else {
+    console.log('Synthetics API Error!');
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(response.statusCode);
+    }
+  }
+}
+
 // Run the queries
+console.log('Details from the API calls');
 api.applicationsList(appCB);
 insights.query(nrql, insightsCB);
+synthetics.getAllMonitors(syntheticsCB);

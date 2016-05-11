47029dd79f28704abc77fb5e50e315e3b333c463
Ken Ahrens <ken.ahrens@gmail.com>
2016-05-11 08:46:21 -0400

    adding callback helper

--- index.js
+++ index.js
@@ -1,67 +1,59 @@
 var api = require('./lib/api.js');
 var insights = require('./lib/insights.js');
 var synthetics = require('./lib/synthetics.js');
+var helper = require('./lib/helper.js');
 
 // Callback
 var appCB = function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    console.log('\n*** APM API output:');
-    var applications = body.applications;
-    for(var i=0; i < applications.length; i++) {
-      var app = applications[i];
-      var id = app.id;
-      var name = app.name;
-      var health = app.health_status;
+  var body = helper.handleCB(error, response, body);
+  console.log('\n*** Applications API output:');
+  var applications = body.applications;
+  for(var i=0; i < applications.length; i++) {
+    var app = applications[i];
+    var id = app.id;
+    var name = app.name;
+    var health = app.health_status;
 
-      console.log(name + '(' + id + ') is ' + health);
-    }
-  } else {
-    console.log('Application API Error!');
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(response.statusCode);
-    }
+    console.log(name + '(' + id + ') is ' + health);
+  }
+}
+
+var serverCB = function(error, response, body) {
+  var body = helper.handleCB(error, response, body);
+  console.log('\n*** Servers API output:');
+  var servers = body.servers;
+  for (var i=0; i < servers.length; i++) {
+    var server = servers[i];
+    var id = server.id;
+    var name = server.name;
+    var health = server.health_status;
+
+    console.log(name + ' (' + id + ') is ' + health);
   }
 }
 
 var nrql = 'SELECT count(*) FROM Transaction';
 var insightsCB = function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    console.log('\n*** Insights API output:');
-    console.log('Transaction count = ' + body.results[0].count);
-  } else {
-    console.log('Insights API Error!');
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(response.statusCode);
-    }
-  }
+  var body = helper.handleCB(error, response, body);
+  console.log('\n*** Insights API output:');
+  console.log('Transaction count = ' + body.results[0].count);
 }
 
 var syntheticsCB = function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    console.log('\n*** Synthetics API output:');
-    var monitors = body.monitors;
-    for (var i=0; i < monitors.length; i++) {
-      var name = monitors[i].name;
-      var status = monitors[i].status;
+  var body = helper.handleCB(error, response, body);
+  console.log('\n*** Synthetics API output:');
+  var monitors = body.monitors;
+  for (var i=0; i < monitors.length; i++) {
+    var name = monitors[i].name;
+    var status = monitors[i].status;
 
-      console.log(name + ' (' + status + ')');
-    }
-  } else {
-    console.log('Synthetics API Error!');
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(response.statusCode);
-    }
+    console.log(name + ' (' + status + ')');
   }
 }
 
 // Run the queries
 console.log('Details from the API calls');
 api.applicationsList(appCB);
+api.serversList(serverCB);
 insights.query(nrql, insightsCB);
 synthetics.getAllMonitors(syntheticsCB);

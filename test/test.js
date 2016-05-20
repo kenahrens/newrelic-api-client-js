196a0e0f23068b6a89e897dc1c408b0acb7cf14c
Ken Ahrens <ken.ahrens@gmail.com>
2016-05-20 19:56:54 -0400

    test reformatting

--- test/test.js
+++ test/test.js
@@ -4,18 +4,33 @@ var synthetics = require('../lib/synthetics.js');
 
 var assert = require('assert');
 
+var quickAssert = function(error, response) {
+  assert.equal(error, null);
+  assert.equal(response.statusCode, 200);
+}
+
 describe('newrelic-api-test', function() {
   it('calls the applications api', function(done) {
     api.applications.list(function(error, response, body) {
-      assert.equal(error, null);
-      assert.equal(response.statusCode, 200);
+      quickAssert(error, response);
       done();
+    });
+  });
+  it('gets a specific application', function(done) {
+    api.applications.list(function(error, response, body) {
+      quickAssert(error, response);
+
+      // Get the first app in the list
+      var appId = body.applications[0].id;
+      api.applications.show(appId, function(error, response, body) {
+        quickAssert(error, response);
+        done();
+      });
     });
   });
   it('calls the servers api', function(done) {
     api.servers.list(function(error, response, body) {
-      assert.equal(error, null);
-      assert.equal(response.statusCode, 200);
+      quickAssert(error, response);
       done();
     });
   });
@@ -24,8 +39,7 @@ describe('newrelic-api-test', function() {
 describe('synthetics-test', function() {
   it('calls the synthetics api', function(done) {
     synthetics.getAllMonitors(function(error, response, body) {
-      assert.equal(error, null);
-      assert.equal(response.statusCode, 200);
+      quickAssert(error, response);
       done();
     })
   });
@@ -35,8 +49,7 @@ describe('insights-test', function() {
   it('calls the query api', function(done) {
     var nrql = 'SELECT count(*) FROM ARG';
     insights.query(nrql, function(error, response, body) {
-      assert.equal(error, null);
-      assert.equal(response.statusCode, 200);
+      quickAssert(error, response);
       done();
     });
   });

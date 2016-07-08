50b6cdbe9346a4ae0cbb1744794c8180a51c428d
Ken Ahrens <ken.ahrens@gmail.com>
2016-07-08 17:52:04 -0400

    adding synthetics get monitor call

--- test/test-synthetics.js
+++ test/test-synthetics.js
@@ -6,11 +6,21 @@ var quickAssert = function(error, response) {
   assert.equal(response.statusCode, 200);
 }
 
+var syntheticsId = 0;
+
 describe('New Relic Synthetics API Test', function() {
   this.timeout(5000);
   
-  it('calls the synthetics api', function(done) {
+  it('gets the list of all monitors', function(done) {
     synthetics.getAllMonitors(function(error, response, body) {
+      syntheticsId = body.monitors[0].id;
+      quickAssert(error, response);
+      done();
+    })
+  });
+
+  it('gets a single monitor', function(done) {
+    synthetics.getMonitor(syntheticsId, function(error, response, body) {
       quickAssert(error, response);
       done();
     })

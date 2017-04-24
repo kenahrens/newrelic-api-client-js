d3595d690b4f5a16018507149a7b1b373559d3c0
Ken Ahrens <kahrens@newrelic.com>
2017-04-24 14:36:43 -0400

    Version 0.1.0 adding Insights insert API call

--- test/test-insights.js
+++ test/test-insights.js
@@ -13,6 +13,25 @@ var configId = config.get('configArr')[0];
 describe('New Relic Insights API Test', function() {
   this.timeout(5000);
   
+  it('publishes some sample events', function(done) {
+    var eventArr = [];
+    eventArr.push({
+      "eventType":"ZInsertTest",
+      "account":3,
+      "amount":259.54
+    });
+    eventArr.push({
+      "eventType":"ZInsertTest",
+      "account":5,
+      "amount":12309,
+      "product":"Super expensive thing"
+    });
+    insights.publish(eventArr, configId, function(error, response, body) {
+      quickAssert(error, response);
+      done();
+    })
+  });
+
   it('calls the query api', function(done) {
     var nrql = 'SELECT count(*) FROM Transaction';
     insights.query(nrql, configId, function(error, response, body) {

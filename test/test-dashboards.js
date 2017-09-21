bc355787232257beb851d333eec579a42bed55f8
Ken Ahrens <kahrens@newrelic.com>
2017-09-20 20:47:36 -0400

    finishing dashboard crud work

--- test/test-dashboards.js
+++ test/test-dashboards.js
@@ -17,11 +17,52 @@ var pluginId = 0;
 var componentId = 0;
 var configId = config.get('configArr')[0];
 
+// Global variables
+var firstDashboardId = 0;
+var dashboardBody = '';
+var secondDashboardId = 0;
+
 describe('New Relic Dashboards Test', function() {
   this.timeout(15000);
 
   it('gets the list of Insights Dashboards', function(done) {
-    dashboards.list(configId, 1, function(error, response, body) {
+    dashboards.getAll(1, configId, function(error, response, body) {
+      quickAssert(error, response);
+
+      // Get the first dashboard in the list
+      firstDashboardId = body.dashboards[0].id;
+
+      done();
+    });
+  });
+
+  it('gets the first dashboard from the list', function(done) {
+    dashboards.getOne(firstDashboardId, configId, function(error, response, body) {
+      quickAssert(error, response);
+      dashboardBody = body;
+      done();
+    });
+  });
+
+  it('creates a copy of the first dashboard', function(done) {
+    dashboardBody.dashboard.title = 'API Copy of ' + dashboardBody.dashboard.title;
+    dashboards.create(dashboardBody, configId, function(error, response, body) {
+      quickAssert(error, response);
+      secondDashboardId = body.dashboard.id;
+      done();
+    });
+  });
+
+  it('updates the title of the copied dashboard', function(done) {
+    dashboardBody.dashboard.title = 'API Update of ' + dashboardBody.dashboard.title;
+    dashboards.update(secondDashboardId, dashboardBody, configId, function(error, response, body) {
+      quickAssert(error, response);
+      done();
+    });
+  });
+
+  it('deletes the copied dashboard', function(done) {
+    dashboards.delete(secondDashboardId, configId, function(error, response, body) {
       quickAssert(error, response);
       done();
     });

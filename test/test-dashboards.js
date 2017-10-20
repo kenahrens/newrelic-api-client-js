5902c550a3a109e85dd939ef4adba69b0252eeee
Ken Ahrens <kahrens@newrelic.com>
2017-10-20 16:47:39 -0400

    enabling tests

--- test/test-dashboards.js
+++ test/test-dashboards.js
@@ -22,47 +22,47 @@ var secondDashboardId = 0;
 describe('New Relic Dashboards Test', function() {
   this.timeout(15000);
 
-  // it('gets the first page of Insights Dashboards', function(done) {
-  //   dashboards.getPage(1, configId, function(error, response, body) {
-  //     quickAssert(error, response);
+  it('gets the first page of Insights Dashboards', function(done) {
+    dashboards.getPage(1, configId, function(error, response, body) {
+      quickAssert(error, response);
 
-  //     // Get the first dashboard in the list
-  //     firstDashboardId = body.dashboards[0].id;
+      // Get the first dashboard in the list
+      firstDashboardId = body.dashboards[0].id;
 
-  //     done();
-  //   });
-  // });
+      done();
+    });
+  });
 
-  // it('gets the first dashboard from the list', function(done) {
-  //   dashboards.getOne(firstDashboardId, configId, function(error, response, body) {
-  //     quickAssert(error, response);
-  //     dashboardBody = body;
-  //     done();
-  //   });
-  // });
+  it('gets the first dashboard from the list', function(done) {
+    dashboards.getOne(firstDashboardId, configId, function(error, response, body) {
+      quickAssert(error, response);
+      dashboardBody = body;
+      done();
+    });
+  });
 
-  // it('creates a copy of the first dashboard', function(done) {
-  //   dashboardBody.dashboard.title = 'API Copy of ' + dashboardBody.dashboard.title;
-  //   dashboards.create(dashboardBody, configId, function(error, response, body) {
-  //     quickAssert(error, response);
-  //     secondDashboardId = body.dashboard.id;
-  //     done();
-  //   });
-  // });
+  it('creates a copy of the first dashboard', function(done) {
+    dashboardBody.dashboard.title = 'API Copy of ' + dashboardBody.dashboard.title;
+    dashboards.create(dashboardBody, configId, function(error, response, body) {
+      quickAssert(error, response);
+      secondDashboardId = body.dashboard.id;
+      done();
+    });
+  });
 
-  // it('updates the title of the copied dashboard', function(done) {
-  //   dashboardBody.dashboard.title = 'API Update of ' + dashboardBody.dashboard.title;
-  //   dashboards.update(secondDashboardId, dashboardBody, configId, function(error, response, body) {
-  //     quickAssert(error, response);
-  //     done();
-  //   });
-  // });
+  it('updates the title of the copied dashboard', function(done) {
+    dashboardBody.dashboard.title = 'API Update of ' + dashboardBody.dashboard.title;
+    dashboards.update(secondDashboardId, dashboardBody, configId, function(error, response, body) {
+      quickAssert(error, response);
+      done();
+    });
+  });
 
-  // it('deletes the copied dashboard', function(done) {
-  //   dashboards.delete(secondDashboardId, configId, function(error, response, body) {
-  //     quickAssert(error, response);
-  //     done();
-  //   });
-  // });
+  it('deletes the copied dashboard', function(done) {
+    dashboards.delete(secondDashboardId, configId, function(error, response, body) {
+      quickAssert(error, response);
+      done();
+    });
+  });
 
 });

690e897dec1a3f40a446e120b0872e57889ebd91
Adilson Somensari <asomensari@newrelic.com>
2018-01-12 15:55:49 -0500

    Add dashboard and Alerts to Module Export

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var api = require('./lib/api.js');
 var insights = require('./lib/insights.js');
 var synthetics = require('./lib/synthetics.js');
 var partners = require('./lib/partners.js');
+var dashboards = require('./lib/dashboards.js');
+var alerts = require('./lib/alerts.js');
 
 module.exports = {
   api: api,
   insights: insights,
   synthetics: synthetics,
-  partners: partners
+  partners: partners,
+  dashboards: dashboards,
+  alerts: alerts
 }

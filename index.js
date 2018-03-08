f0c453053bf2cbd24f7c9d50c30ee4497db3ae5b
Ken Ahrens <kahrens@newrelic.com>
2018-03-08 18:54:41 -0500

    updating to version 0.1.3

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var synthetics = require('./lib/synthetics.js');
 var partners = require('./lib/partners.js');
 var dashboards = require('./lib/dashboards.js');
 var alerts = require('./lib/alerts.js');
+var helper = require('./lib/helper.js');
 
 module.exports = {
   api: api,
@@ -11,5 +12,6 @@ module.exports = {
   synthetics: synthetics,
   partners: partners,
   dashboards: dashboards,
-  alerts: alerts
+  alerts: alerts,
+  helper: helper
 }

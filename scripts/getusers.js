c7426121ce50ee188e6efaaec7386436700f6bc1
Ken Ahrens <kahrens@newrelic.com>
2018-03-27 15:13:25 -0400

    updating user script

--- scripts/getusers.js
+++ scripts/getusers.js
@@ -1,3 +1,5 @@
+// Get all the users for an account, output as CSV
+
 const config = require('config');
 
 var api = require('../lib/api.js');
@@ -6,16 +8,36 @@ var helper = require('../lib/helper.js');
 var json2csv = require('json2csv');
 const fs = require('fs');
 
-// Get all the users for an account, output as CSV
-var totalAccountCount = 0;
+// Script-wide variables
+var cfgList = config.get('configArr');
+var totalAccountCount = cfgList.length;
 var currentAccountCount = 0;
 var userArr = [];
 
+function makeFields() {
+  
+  // Standard fields
+  var fieldArr = ['userId', 'first_name', 'last_name', 'email'];
+  
+  // Add 1 field {accountId}_role
+  for (var i=0; i < cfgList.length; i++) {
+    var configId = cfgList[i];
+    var accountId = config.get(configId + '.accountId');
+    var colName = accountId + '_role';
+    fieldArr.push(colName);
+  }
+  return fieldArr;
+}
+
 function toCSV() {
+  
+  // Setup the input
   var input = {
     data: userArr,
-    fields: ['accountId', 'userId', 'first_name', 'last_name', 'email', 'role']
+    fields: makeFields()
   }
+
+  // Store the CSV
   json2csv(input, function(err, csvData) {
     if (err) {
       console.log('ERROR!');
@@ -32,6 +54,26 @@ function toCSV() {
   });
 }
 
+function parseUser(user, accountId) {
+  // Lookup the user
+  var newUser = userArr[user.id];
+  if (newUser == null) {
+    newUser = {
+      userId: user.id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email
+    }
+  }
+
+  // Add this accountId role
+  var colName = accountId + '_role';
+  newUser[colName] = user.role;
+
+  // Add the new user to the array
+  userArr[newUser.userId] = newUser;
+}
+
 function parseResponse(configId, handledBody) {
   // Lookup the accountId
   var accountId = config.get(configId + '.accountId');
@@ -39,11 +81,11 @@ function parseResponse(configId, handledBody) {
   // Make sure we got a valid response
   if (handledBody != null) {
     var userList = handledBody.users;
-    userList.forEach(function(user) {
-      user.accountId = accountId;
-      user.userId = user.id;
-      userArr.push(user);
-    });
+    console.log('Parsing', userList.length, 'users for', configId);
+    for (var i=0; i < userList.length; i++) {
+      var user = userList[i];
+      parseUser(user, accountId);
+    }
   }
 
   // Check if we got all counts
@@ -53,9 +95,6 @@ function parseResponse(configId, handledBody) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-var cfgList = config.get('configArr');
-totalAccountCount = cfgList.length;
-
 console.log('Geting users across', totalAccountCount, 'accounts.');
 cfgList.forEach(function(configId) {
   api.users.list(null, configId, function(error, response, body) {
